2b018f90f1681a3d99932692de25301358d104b5
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-18 16:47:09 +0900

    ko

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'product/index'
   root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :product, only: [:index,]
 end
