f684006cbbe500248659ee1561a98caec7222a37
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-20 14:44:51 +0900

    ルートとコントローラアクションの修正
    deviseの機能に関連するルーティングは自動で設定されているので、showのみを手動で定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#show'
   resources :products, only: [:index]
-  #ユーザーモデルでは一つのリソースしか参照しないため、単数形にしています。もし、管理者権限などでuser#indexを使ったり、user名を検索するならば複数系にする必要があります。現状そこはなさそう。宮城
-  #マイページ経由で
-  resource :user, except: [:index,:destroy]
+  resource :user, only: [:show]
 end
