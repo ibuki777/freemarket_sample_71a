854a5b546a4d2b4735e226c8810ac5e611bd9e72
ibuki777 <hanahana7802@gmail.com>
2020-03-24 21:29:15 +0900

    途中経過

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index,:show]
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :show, :new, :create]
 
 end
