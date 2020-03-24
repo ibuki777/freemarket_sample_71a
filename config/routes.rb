f006d6c79b32316f158240166fa86e7c91e909e8
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-24 21:11:47 +0900

    プロフィール作成のためのroutes づくり

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#show'
   resources :products, only: [:index]
-  resource :user, only: [:show] do
+  resource :user, only: [:show, :edit, :update] do
     collection do
       get'logout'
     end
