0f5a138cd2a688320f20937d0cd28fb0a2a24e0d
ibuki777 <hanahana7802@gmail.com>
2020-03-25 21:01:44 +0900

    微調整

--- app/models/deliveryday.rb
+++ app/models/deliveryday.rb
@@ -1,6 +1,6 @@
 class Deliveryday < ActiveHash::Base
 
   self.data = [
-    {id: 1, name: '選択がありません'},{id: 2, name: '1〜2日で発送'},{id: 3, name: '2〜3日で発送'},{id: 4, name: '4〜7日で発送'}
+    {id: 1, name: '1〜2日で発送'},{id: 2, name: '2〜3日で発送'},{id: 3, name: '4〜7日で発送'}
   ]
 end
