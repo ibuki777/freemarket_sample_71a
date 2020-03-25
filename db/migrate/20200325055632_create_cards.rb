94917c1c5293522a18ee62b8ec52cc4465a03b4c
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-25 15:00:04 +0900

    edit cards migrate file

--- db/migrate/20200325055632_create_cards.rb
+++ db/migrate/20200325055632_create_cards.rb
@@ -1,10 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.integer :user_id
-      t.string :customer_id
-      t.string :card_id
-
+      t.integer :user_id, null: false
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
       t.timestamps
     end
   end
