05e81d7c0cc5144f10d748c94e04963913953231
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-25 16:50:04 +0900

    バリデーション(user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, :birthday, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/}
+
   has_many :addresses
 end
