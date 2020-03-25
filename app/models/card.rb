078c7a5d62afc60b4d70385dbc7e8a33588dd4d1
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-25 14:59:07 +0900

    generate card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,20 +1,2 @@
 class Card < ApplicationRecord
-
-  require 'payjp'
-  Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-
-  def self.create_card_to_payjp(params)
-    # トークンを作成 
-    token = Payjp::Token.create({
-      card: {
-        number:     params['number'],
-        cvc:        params['cvc'],
-        exp_month:  params['valid_month'],
-        exp_year:   params['valid_year']
-      }},
-      {'X-Payjp-Direct-Token-Generate': 'true'} 
-    )
-    # 上記で作成したトークンをもとに顧客情報を作成
-    Payjp::Customer.create(card: token.id)
-  end
 end
