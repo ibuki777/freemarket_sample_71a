3f7b9b16bad057222aa952a67bc79073423f1432
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-25 20:04:20 +0900

    v

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # validates :name, {presence:true,length: {maximum:40}}
   validates :name, {presence:{message: "を４０字以内で入力してください"},length: {maximum:40}}
   validates :explain, {presence:{message: "を入力、または1000字以内で入力してください"},length: {maximum:1000}}
-
-  belongs_to_active_hash :category
+  validates :price, 
+            :numericality => { 
+            :greater_than_or_equal_to => 299,
+            :less_than_or_equal_to => 1000000,
+            :message => '300以上9999999以下で入力してください'
+            }
+  # belongs_to_active_hash :category
   belongs_to :brand
   has_many :images, dependent: :destroy
 end
