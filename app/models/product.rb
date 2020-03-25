228638e55bde18c00d19f9a619bbb87d715a207f
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-25 19:25:11 +0900

    l

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :name, {presence:true,length: {maximum:40}}
+  # validates :name, {presence:true,length: {maximum:40}}
   validates :name, {presence:{message: "を４０字以内で入力してください"},length: {maximum:40}}
   validates :explain, {presence:{message: "を入力、または1000字以内で入力してください"},length: {maximum:1000}}
 
