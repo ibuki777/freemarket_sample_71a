a6a9e9a8aa7baa3d89856cb29a0e7af3bec82332
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-25 16:35:17 +0900

    ウィザード途中

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.addresses.build
+    render :new_address
+  end
 
   # GET /resource/edit
   # def edit
