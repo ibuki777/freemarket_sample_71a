ff068d53824b0060d0d47845ba1e51571ff5b524
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-25 17:01:11 +0900

    アクティブハッシュ導入

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
+  validates :zip_code, :city, :address, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
 end
