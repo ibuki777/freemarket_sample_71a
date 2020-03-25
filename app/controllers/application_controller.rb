054410ac30c1fe820187bd1f7ac08e35f05826b3
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-25 16:52:42 +0900

    user内カラム保存許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,10 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
-  
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
+
 end
