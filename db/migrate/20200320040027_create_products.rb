2f0acecdb2ff52192bb1f7e53d49bb13489b81f2
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-25 17:40:45 +0900

    Update 20200320040027_create_products.rb

--- db/migrate/20200320040027_create_products.rb
+++ db/migrate/20200320040027_create_products.rb
@@ -4,6 +4,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :name, null: false,length: {maximum: 40}
       t.text :explain,null: false,length: {maximum: 1000}
       t.integer :price, null: false
+      t.integer :prefecture_id, null: false
       t.timestamps
     end
   end
