f5df08724e2ef96244b74a1d28bb2bca402f0ab7
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-25 17:10:08 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-  validates :name, presence:{message: "を４０字以内で入力してください"},length: {maximum:40}
-  
+  validates :name, {presence:{message: "を４０字以内で入力してください"},length: {maximum:40}}
+  validates :explain, {presence:{message: "を入力、または1000字以内で入力してください"},length: {maximum:1000}}
+
   belongs_to :brand
   belongs_to :category
   has_many :images, dependent: :destroy
