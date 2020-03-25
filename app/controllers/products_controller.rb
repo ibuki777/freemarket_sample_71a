806aad220e3ef35eefac68b11f07e7323387d273
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-25 15:47:58 +0900

    途中経過

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,11 +10,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+    @product = Product.new
   end
 
   def create
-   
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to new_product_path
+    else
+      render "new"
+    end
   end
 
   def edit
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, presence:true,length: {maximum:40}
+  validates :name, {presence:true,length: {maximum:40}}
   
   belongs_to :brand
   belongs_to :category
