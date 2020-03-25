33f97a9bb4036160ebbc0bb082c37b817f3afc24
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-25 17:28:31 +0900

    Update 20200320040027_create_products.rb

--- db/migrate/20200320040027_create_products.rb
+++ db/migrate/20200320040027_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name, null: false
-      t.text :explain
+      t.string :name, null: false,length: {maximum: 40}
+      t.text :explain,null: false,length: {maximum: 1000}
       t.integer :price, null: false
       t.timestamps
     end
