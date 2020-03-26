325b18308a9770efb4b2b062a4e9fbe337b149c0
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-26 14:31:42 +0900

    ルートの作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,4 +8,12 @@ class CardsController < ApplicationController
   def create
   end
 
+  # カード情報の編集のリクエストを送るのに使うかも
+  # def edit
+  # end
+
+  # カード情報の破棄の際に使用予定
+  # def destroy
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
       get'logout'
     end
   end
-  resources :cards, only: [:index, :new]    
+  resources :cards, only: [:index, :new, :create]
 end
