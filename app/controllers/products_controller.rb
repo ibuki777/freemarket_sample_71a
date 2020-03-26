9028c13828010ba24c9e3637494cb9de82d4990b
nulinulizainuli0515 <61219184+nulinulizainuli0515@users.noreply.github.com>
2020-03-26 16:31:19 +0900

    コミット

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-
+  before_action :set_product, except: [:index, :new, :create]
+  
   def index
     @products = Product.order(created_at: :desc).limit(1)
     @images = Image.all.includes(:product)
@@ -10,23 +11,47 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+    @product = Product.new
+    @product.images.new
   end
 
   def create
-    
+    @product = Product.new(product_params)
+    if @product.save
+    redirect_to root_path
+    else
+    render :new
+    end
   end
 
   def edit
-    
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def update
-    
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
     
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, images_attributes: [:image, :_destroy, :id])
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -31,6 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
+  process resize_to_fit: [100, 100]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
