b9567b9b37e93f02cf4ae61d3aae31ca48a35ec0
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-26 14:51:51 +0900

    単体テスト（アドレス）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
-    it "is valid with a all" do
+    it "必須事項があれば登録できる" do
       user = build(:user)
       expect(user).to be_valid
     end
