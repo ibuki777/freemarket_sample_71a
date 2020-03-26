d8e00e9bdc1b3258fa533bf1d8d4610cbeca2fd7
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-26 11:55:11 +0900

    Update deliveryday.rb

--- app/models/deliveryday.rb
+++ app/models/deliveryday.rb
@@ -1,5 +1,4 @@
 class Deliveryday < ActiveHash::Base
-  validates :id, presence: true
   self.data = [
     {id: 1, name: '選択がありません'},{id: 2, name: '1〜2日で発送'},{id: 3, name: '2〜3日で発送'},{id: 4, name: '4〜7日で発送'}
   ]
