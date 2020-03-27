7a41887a4a9063695ee60611a26dd160fa397be2
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-27 15:22:06 +0900

    途中経過

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,5 @@ class Product < ApplicationRecord
   belongs_to_active_hash :burden
   belongs_to_active_hash :deliveryday
   has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy:true
 end
