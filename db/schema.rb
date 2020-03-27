cb406dab0ee987380218f81e432d23abbb103b56
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-27 12:29:15 +0900

    formブロック構造構築

--- db/schema.rb
+++ db/schema.rb
@@ -37,11 +37,11 @@ ActiveRecord::Schema.define(version: 2020_03_25_072220) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "payjp_id", null: false
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 2020_03_25_072220) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "cards", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
