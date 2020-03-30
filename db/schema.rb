c2ef8e5534536441037519011c0a4ec71ea9360a
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-30 20:05:08 +0900

    db reset & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_065022) do
+ActiveRecord::Schema.define(version: 2020_03_30_100454) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -58,6 +58,16 @@ ActiveRecord::Schema.define(version: 2020_03_30_065022) do
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "shipping_address", null: false
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "explain"
@@ -92,6 +102,8 @@ ActiveRecord::Schema.define(version: 2020_03_30_065022) do
 
   add_foreign_key "cards", "users"
   add_foreign_key "images", "products"
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
 end
