8893d451abc07a00a3b894a046c6a58beb0dbdbb
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-30 17:04:17 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 2020_03_30_065022) do
     t.string "genre", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
