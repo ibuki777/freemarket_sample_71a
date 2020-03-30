44a0fe989bbe4342d060778f13cfd88f116b1e75
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-30 16:33:07 +0900

    各データ表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @images = @product.images
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -16,6 +16,8 @@ class Product < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :burden
   belongs_to_active_hash :deliveryday
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :brand
   has_one :order
   has_many :images
   belongs_to :user
