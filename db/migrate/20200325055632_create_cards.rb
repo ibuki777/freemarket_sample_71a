cfb538f4f3889171e37e6a295e0b414da8ea2756
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-30 20:04:56 +0900

    modify cards tabel

--- db/migrate/20200325055632_create_cards.rb
+++ db/migrate/20200325055632_create_cards.rb
@@ -1,9 +1,8 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.integer :user_id, null: false
-      t.string :customer_id, null: false
-      t.string :card_id, null: false
+      t.references :user, foreign_key: true
+      t.string :payjp_id,null: false 
       t.timestamps
     end
   end
