2011e6cfdec9d5f27d0edc7fbaeeb8fa1380d7e2
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-31 11:42:28 +0900

    購入する商品情報の取得

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :set_product
+
   def new
+    @order = Order.new
+
   end
 
   def create
+   
   end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
 end
