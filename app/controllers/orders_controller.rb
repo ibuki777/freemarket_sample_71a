709d9fe6784c211f050f0d7e441c4b417e9d7a95
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-31 12:28:09 +0900

    購入手続きと注文レコードの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,30 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @amount = @product.price
 
   end
 
   def create
-   
+
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY) 
+      # 請求を発行
+      Payjp::Charge.create(
+      amount: @product.price,
+      customer: @card.customer_id,
+      currency: 'jpy',
+      )
+      # 売り切れなので、productの情報をアップデートして売り切れにします。
+      @order = Order.new(user_id: current_user.id, product_id: @product.id, address_id: current_user.address.id)
+      if @order.save
+        flash[:notice] = '購入しました。'
+        redirect_to controller: 'products', action: 'show', id: @product.id
+      else
+        flash[:alert] = '購入に失敗しました。'
+        redirect_to controller: 'products', action: 'show', id: @product.id
+      end
+    end
+
   end
 
   private
