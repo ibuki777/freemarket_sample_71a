5555162984acfcb619af776a0fc7ecb44011feed
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-01 13:00:45 +0900

    試行錯誤

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
+
   factory :order do
-    address { 1 }
-    product { 1 }
-    user { 1 }
+    address {"1"}
+    product {"1"}
+    user    {"1"}
   end
+  
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,19 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Order, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Order, type: :model do
+  describe '#create' do
+    context 'can save' do
+      it 'is valid with all data' do
+        expect(build(:order)).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it 'is invalid without user_id' do
+        message = build(:order, user: nil )
+        message.valid?
+        expect(message.errors[:product_id]).to include("を入力してください")
+      end
+    end
+  end
+end
