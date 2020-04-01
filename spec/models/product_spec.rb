fb3e137bf2bdb62ee18a4e4e2821c9def9ee2964
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-01 13:45:03 +0900

    テスト

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,19 @@ describe Product do
   describe '#create' do
     it "saves data of activehash" do
       product = build(:product)
+      product.valid?
+    end
+    it "商品名を入力してください" do
+      product = Product.new
+      product.valid?
+    end
+    it "商品説明を入力してください" do
+      product = Product.new
+      product.valid?
+    end
+    it "商品の値段を入力してください" do
+      product = Product.new
+      product.valid?
     end
   end
 end
