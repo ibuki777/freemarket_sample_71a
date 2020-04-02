a14da3dde4e0be1cc995bca549952e981c694c95
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-02 20:16:48 +0900

    途中経過

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,9 @@ class Product < ApplicationRecord
   validates :explain, {presence:{message: "を入力、または1000字以内で入力してください"},length: {maximum:1000}}
   validates :price, 
   :numericality => { 
-    :greater_than_or_equal_to => 299,
-    :less_than_or_equal_to => 1000000,
-    :message => '300以上9999999以下で入力してください'
+    :greater_than_or_equal_to => 300,
+    :less_than_or_equal_to => 999999,
+    :message => '300以上999999以下で入力してください'
   }
   validates :burden_id,:condition_id,:category_id,:deliveryday_id,:prefecture_id,{presence:{message: "を選択してください"}}
 
