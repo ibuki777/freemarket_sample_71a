285551cd2a2e85774312f1c4501044fa9dca9a24
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-02 17:52:57 +0900

    fakerで入力

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
+require "faker"
 
   factory :address do
     last_name             {"あべ"}
     first_name            {"ひろし"}
     last_name_kana        {"あべ"}
     first_name_kana       {"ひろし"}
-    zip_code              {"0000000"}
-    city                  {"大阪市"}
-    address               {"1-11"}
-    prefecture_id         {"38"}
+    zip_code              {rand(0000000..9999999)}
+    city                  {Faker::Address.city}
+    address               {Faker::Address.street_address}
+    prefecture_id         {rand(1..47)}
+    user
   end
+
 end
