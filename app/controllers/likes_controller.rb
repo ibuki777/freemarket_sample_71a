63d04959c3830a0be146f0093bb1626ef1165906
ibuki777 <hanahana7802@gmail.com>
2020-04-03 17:46:53 +0900

    条件分岐の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,18 +4,18 @@ class LikesController < ApplicationController
   def like
     like = current_user.likes.new(product_id: @product.id)
     if like.save
-      flash.now[:alert] = 'お気に入りに登録しました'
+      flash[:alert] = 'お気に入りに登録しました'
     else
-      flash.now[:alert] = 'システムエラー   もう一度お試しください'
+      flash[:alert] = 'システムエラー   もう一度お試しください'
     end
   end
 
   def unlike
     like = current_user.likes.find_by(product_id: @product.id)
     if like.destroy
-      flash.now[:alert] = 'お気に入りから解除しました'
+      flash[:alert] = 'お気に入りから解除しました'
     else
-      flash.now[:alert] = 'システムエラー   もう一度お試しください'
+      flash[:alert] = 'システムエラー   もう一度お試しください'
     end
   end
 
