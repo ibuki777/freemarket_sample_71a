6e9c8693f3725151a41959e5e04dc57bfff0967c
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-03 11:54:30 +0900

    card情報のバリデーションテスト

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -8,11 +8,11 @@ describe Card do
       expect(card).to be_valid
     end
 
-    # it "user_id がないと保存できない。" do
-    #   card = build(:card, user_id: nil)
-    #   card.valid?
-    #   expect(card.errors[:user_id]).to include("を入力してください")
-    # end
+    it "user_id がないと保存できない。" do
+      card = build(:card, user_id: nil)
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
+    end
   end
 
 
