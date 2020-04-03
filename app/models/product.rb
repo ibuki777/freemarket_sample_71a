6e5ac5915717a49ef6ab9621051312b396e4c28f
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-03 15:57:05 +0900

    モデル設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,5 +23,11 @@ class Product < ApplicationRecord
   has_many :products
   belongs_to :user
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  def self.search(search)
+    return Product.all unless search
+    Product.where(['name LIKE ?', "%#{search}%"])
+  end
+  
 end
 
