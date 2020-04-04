a807e8007c7bb217a135046c5f73fd1af675ae0b
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-04 22:42:01 +0900

    ルート、コントローラー設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,7 +33,24 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    
+    grandchild_category = @product.category
+    child_category = grandchild_category.parent
+
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
   end
 
   def update
