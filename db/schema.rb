f0a490040cc06ed79772863a3e924727c4ffe282
ibuki777 <hanahana7802@gmail.com>
2020-04-07 16:34:34 +0900

    schema.rb　書き換え

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_090015) do
-
+ActiveRecord::Schema.define(version: 2020_04_06_085112) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -98,8 +97,8 @@ ActiveRecord::Schema.define(version: 2020_04_02_090015) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "exhibition_id", null: false
     t.integer "likes_count"
-    t.integer "exhibition_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
