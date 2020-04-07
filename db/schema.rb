eff21bdfdbb266600ede8a453a2b53230aa9285e
nulinulizainuli0515 <61219184+nulinulizainuli0515@users.noreply.github.com>
2020-04-07 14:41:19 +0900

    1111

--- db/schema.rb
+++ db/schema.rb
@@ -67,11 +67,12 @@ ActiveRecord::Schema.define(version: 2020_04_06_085112) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "shipping_address", null: false
+    t.bigint "address_id", null: false
     t.bigint "product_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_orders_on_address_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -114,6 +115,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_085112) do
 
   add_foreign_key "cards", "users"
   add_foreign_key "images", "products"
+  add_foreign_key "orders", "addresses"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
