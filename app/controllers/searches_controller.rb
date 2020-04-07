87faa2c3737911f1671bf348012b4b985ed0562b
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-07 20:45:05 +0900

    ビュー

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,4 @@
 class SearchesController < ApplicationController
   def index
-    # @products = Product.search(params[:keyword]).page(params[:page]).per(9).order("created_at DESC")
-    # @search = params[:keyword]
   end
 end
