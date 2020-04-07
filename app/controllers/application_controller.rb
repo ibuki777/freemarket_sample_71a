f1cfbb551eb2cfc3bdb2fa2ffadcc97368ef26d5
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-07 17:38:10 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   before_action :search
   private
 
@@ -18,6 +17,11 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
+
+  def search
+    @products = Product.ransack(params[:q])
+    @search = @products.result(distinct: true)
   end
 
 end
