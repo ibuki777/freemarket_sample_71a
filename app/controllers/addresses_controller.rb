2c6b89f7861087745ff93a2f3f442650dc341f2c
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-08 12:41:06 +0900

    メソッド化

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,10 @@
 class AddressesController < ApplicationController
+  before_action :set_address
 
   def edit
-    @address = Address.find(params[:id])
   end
 
   def update
-    @address = Address.find(params[:id])
     if current_user.id == @address.user.id && @address.update(address_params)
       redirect_to user_path(current_user)
     else
@@ -15,6 +14,9 @@ class AddressesController < ApplicationController
 
   private
 
+  def set_address
+    @address = Address.find(params[:id])
+  end
 
   def address_params
     params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip_code, :city, :address, :building, :phone_number, :prefecture_id)
