069e17aef81817ca0256d27b8d66ad6e0fdf28ec
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-08 15:54:03 +0900

    出品停止中から出品停止へと表示の変更

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,5 +1,5 @@
 class Exhibition < ActiveHash::Base
   self.data = [
-    {id: 1, name: '売り切れ'},{id: 2, name: '出品中'},{id: 3, name: '出品停止中'}
+    {id: 1, name: '売り切れ'},{id: 2, name: '出品中'},{id: 3, name: '出品停止'}
   ]
 end
