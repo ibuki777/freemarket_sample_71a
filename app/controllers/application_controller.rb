f629149d35722e53942401e3469d8f4b9eeeadf0
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-08 19:12:33 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 
+  
   def search
     @products = Product.ransack(params[:q])
     @search = @products.result(distinct: true).where(exhibition_id:(1..2)).page(params[:page]).per(9).order("created_at DESC")
